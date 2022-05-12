470f10c60025d37059ab164015964b10b815ed37
maticjesenicnik <matic.jesenicnik@gmail.com>
2022-05-12 10:24:20 +0200

    feat: json-server
    Added a json server with default data, and the ability to fetch people from the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+interface IState {
+    people: {
+        name: string
+        age: number
+        url: string
+        note?: string
+    }[]
+}
 
 function App() {
-  return (
-    <div className="container">
+    const [people, setPeople] = useState<IState["people"]>([]);
 
+    useEffect(() => {
+        fetchPeople().then(res => setPeople(res))
+    }, [])
+
+    const fetchPeople = async () => {
+        const res = await fetch('http://localhost:5000/people')
+        return await res.json()
+    }
+
+    return (
+    <div className="container">
+      <h1>People that I want to invite</h1>
     </div>
   );
 }
